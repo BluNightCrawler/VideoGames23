0fdf4dc7b933e7399e70cb42597b2db3002f876e
BluNightCrawler <jeremyfrance123@hotmail.com>
2023-10-17 19:34:14 -0700

    Add project files.

---
+++ VideoGames23/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+namespace VideoGames23.Models
+{
+    public class Product
+    {
+        public long ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+
+        [Column(TypeName="decimal(8,2")]
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+
+        public string System { get; set; }
+    }
+}
